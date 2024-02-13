e20f96512161a9e7b1bc061bab7bbd5ee68e36c9
JTaeger <justust89@gmail.com>
2024-02-13 09:27:55 +0100

    first stuff

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import Layout from './components/Layout/Layout.vue';
+import Login from './components/Login.vue';
+import './styles/index.scss';
+
+</script>
+
+<template>
+  <Layout>
+    <Login></Login>
+  </Layout>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/Layout/Layout.vue
@@ -0,0 +1,38 @@
+<template>
+  <Header />
+
+  <div class="layout">
+    <div class="layout__content">
+
+      <section class="about">
+        <h2>About</h2>
+        <p>
+          A web-based tool to log your patients medication
+        </p>
+
+        <h2>How-to</h2>
+        <p>
+          Log in with your DZD-Login and start entering data
+        </p>
+      </section>
+
+      <slot />
+    </div>
+  </div>
+  <Footer />
+</template>
+<script>
+  import Footer from './Footer.vue';
+  import Header from './Header.vue';
+  export default {
+    name: 'Layout',
+    components: {
+      Footer, Header,
+    }
+  }
+</script>
+<style lang="scss">
+  .layout {
+    padding-top: 0 !important;
+  }
+</style>
