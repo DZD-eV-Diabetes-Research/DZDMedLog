1cb85197ae003bba97eae1bb687c8004a72f9951
JTaeger <justust89@gmail.com>
2024-02-14 09:12:14 +0100

    stuff

--- frontend/src/components/UI/NotFound.vue
+++ frontend/src/components/UI/NotFound.vue
@@ -2,7 +2,6 @@
         <base-card>
             <h2>Sorry the page you were looking for was not found</h2>
             <p>Return to <a href="/">startpage</a></p>
-
         </base-card>
 </template>
 
