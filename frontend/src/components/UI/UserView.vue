71c2b4b1f7a889ac21c90178fe0e7bee2e3698f3
JTaeger <justust89@gmail.com>
2024-02-27 09:31:37 +0100

    minor fixes

--- frontend/src/components/UI/UserView.vue
+++ frontend/src/components/UI/UserView.vue
@@ -1,13 +1,10 @@
 <template>
     <base-card>
         <h1>Hi {{ userStore.get_user_name }} this is what you should see now</h1>
-        <h3>access {{ tokenStore.get_access_token }}</h3>
         <p>This should print your email: {{ userStore.get_email }}</p>
         <p>This should print your display_name: {{ userStore.get_display_name }}</p>
         <p>This should print your roles: {{ userStore.get_roles }}</p>
         <button @click="logout">logout</button>
-        <button @click="userMe">userMe</button>
-        <h1>{{ tokenStore.is_logged_in }}</h1>
     </base-card>
 </template>
 
