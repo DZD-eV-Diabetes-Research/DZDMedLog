10dbe881d0e249f005d8a805a90704599c953df3
JTaeger <justust89@gmail.com>
2024-04-03 11:41:43 +0200

    test

--- MedLog/backend/medlogserver/db/__init__.py
+++
@@ -1,7 +0,0 @@
-from medlogserver.db.event import EventCRUD
-from medlogserver.db.intake import IntakeCRUD
-from medlogserver.db.interview import InterviewCRUD
-from medlogserver.db.study import StudyCRUD
-from medlogserver.db.study_permission import StudyPermissonCRUD
-from medlogserver.db.user import UserCRUD
-from medlogserver.db.user_auth import UserAuthCRUD
