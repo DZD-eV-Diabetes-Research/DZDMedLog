9cd840247408e6492dc649c008a258b9c2d128a7
JTaeger <justust89@gmail.com>
2024-05-10 08:55:49 +0200

    some optimization

--- MedLog/frontend/pages/studies/[id].vue
+++ MedLog/frontend/pages/studies/[id].vue
@@ -1,7 +1,7 @@
 <template>
     <Layout>
         <UIBaseCard style="text-align: center">
-            <h3>Hier kommen jetzt die interviews</h3>
+            <h3>{{ study.display_name }}</h3>
         </UIBaseCard>
     </Layout>    
 </template>
@@ -9,22 +9,12 @@
 <script setup lang="ts">
 
 const studyStore = useStudyStore()
+
 const route = useRoute()
+const study = studyStore.getStudy(route.params.id)
 
-const study = computed(() => {
-    const studyId = route.params.id;
-    return studyStore.studies.items.find(({ id }: { id: string }) => id === studyId);
-});
-
-function test(){
-    console.log("test");
-}
 
 </script>
 
 <style scoped>
-.base-card:hover {
-    background-color: #ededed;
-    cursor: pointer;
-}
 </style>
