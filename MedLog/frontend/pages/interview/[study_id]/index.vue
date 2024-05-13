879d971bb1dec778eaf12b64eca1c71a4cc21e90
JTaeger <justust89@gmail.com>
2024-05-13 15:21:33 +0200

    wip

--- MedLog/frontend/pages/interview/[study_id]/index.vue
+++ MedLog/frontend/pages/interview/[study_id]/index.vue
@@ -1,32 +1,25 @@
 <template>
     <Layout>
         <UIBaseCard @click="newInterview(item)" v-for="item in [...events.items].reverse()" style="text-align: center">
-            <h3>{{stringDoc(item.name)}}</h3>
+            <h3>{{ useStringDoc(item.name) }}</h3>
         </UIBaseCard>
         <UIBaseCard v-if="userStore.isAdmin" class="noHover">
             <UButton @click="showModal = true" label="Event anlegen" color="green" variant="soft"
                 class="border border-green-500 hover:bg-green-300 hover:border-white hover:text-white" />
             <UModal v-model="showModal">
                 <div class="p-4" style="text-align: center;">
-                    <UForm :schema="schema"
-                            :state="state"
-                            class="space-y-4"
-                            @submit="createEvent">
+                    <UForm :schema="schema" :state="state" class="space-y-4" @submit="createEvent">
                         <h3>Event anlegen</h3>
-                        <UFormGroup label="Display Name" name="display_name">
-                            <UInput v-model="state.display_name" required />
+                        <UFormGroup label="Event Name" name="name">
+                            <UInput v-model="state.name" required placeholder="Interview Campaign Year Quarter" />
                         </UFormGroup>
-                        <UFormGroup label="Abkürzing" name="abbreviation">
-                            <UInput v-model="state.abbreviation" required />
-                        </UFormGroup>
-                        <UCheckbox v-model="state.deactivated" name="deactivated" label="Deactivated" />
                         <UButton type="submit" label="Event anlegen" color="green" variant="soft"
                             class="border border-green-500 hover:bg-green-300 hover:border-white hover:text-white" />
                     </UForm>
                 </div>
             </UModal>
         </UIBaseCard>
-    </Layout>    
+    </Layout>
 </template>
 
 <script setup lang="ts">
@@ -34,50 +27,35 @@
 import { object, string, type InferType } from "yup";
 
 
-const showModal = ref(false)
-const state = reactive({
-    display_name: "",
-    deactivated: false,
-    no_permissions: false,
-    id: "",
-    abbreviation: ""
-});
-
-const schema = object({
-    display_name: string().required("Required"),
-    abbreviation: string().required("Required"),
-});
-
-function createEvent() {
-    showModal.value = false
-    console.log(state.display_name.trim());
-    console.log(state.abbreviation.trim().replace(" ", ""));
-    console.log(state.deactivated)
-}
-
 const userStore = useUserStore()
 const tokenStore = useTokenStore()
 const route = useRoute()
 const router = useRouter()
 
+const showModal = ref(false)
+const state = reactive({ name: "" });
+
+const schema = object({
+    name: string().required("Required"),
+});
 
 const { data: events } = await useFetch(`http://localhost:8888/study/${route.params.study_id}/event`, {
     method: "GET",
     headers: { 'Authorization': "Bearer " + tokenStore.access_token },
 })
 
-function newInterview(item){
-    router.push({ path: "/interview/" + route.params.study_id + '/event/' +  item.id})
+function newInterview(item) {
+    router.push({ path: "/interview/" + route.params.study_id + '/event/' + item.id })
 }
 
-const stringDoc = (ugly_name: string): string => {
-    let beautiful_name = ugly_name.replaceAll("-"," ")
-    const words = beautiful_name.split(" ");
-        const capitalizedWords = words.map(word => {
-        return word.charAt(0).toUpperCase() + word.slice(1);
-    });
-        return capitalizedWords.join(" ");
-};
+function createEvent() {
+
+    useCreateEvent(state.name.trim(), route.params.study_id)
+    
+    console.log(route.params.study_id);
+    console.log(state.name.trim());
+    showModal.value = false
+}
 
 </script>
 
@@ -86,9 +64,9 @@ const stringDoc = (ugly_name: string): string => {
     background-color: #ededed;
     cursor: pointer;
 }
+
 .noHover:hover {
     background-color: white;
     cursor: default;
 }
-
 </style>
