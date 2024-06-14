02b93c6b3679b586ef8aaa2755e8298160be6ff7
JTaeger <justust89@gmail.com>
2024-06-14 19:45:51 +0200

    wip

--- MedLog/frontend/pages/interview/proband/[proband_id]/study/[study_id]/index.vue
+++ MedLog/frontend/pages/interview/proband/[proband_id]/study/[study_id]/index.vue
@@ -1,12 +1,20 @@
 <template>
-     <Layout>
-       <p style="text-align: center;"> hi from the other side
-      {{ route.params }}</p>
-</Layout>
+  <Layout>
+    <UIBaseCard v-for="event in events.items" :key="event.id">
+      <p>{{ event.name }}</p>
+    </UIBaseCard>
+  </Layout>
 </template>
 
 <script setup lang="ts">
 
 const route = useRoute()
+const runtimeConfig = useRuntimeConfig()
+const tokenStore = useTokenStore()
+
+const { data: events, refresh } = await useFetch(`${runtimeConfig.public.baseURL}study/${route.params.study_id}/event`, {
+    method: "GET",
+    headers: { 'Authorization': "Bearer " + tokenStore.access_token },
+})
 
 </script>
