6b96d8dfd8bbdfdfdfee1f4aa6e0ee89d1f7d9cc
JTaeger <justust89@gmail.com>
2024-08-29 13:47:07 +0200

    working again

--- MedLog/frontend/nuxt.config.ts
+++ MedLog/frontend/nuxt.config.ts
@@ -5,35 +5,45 @@ export default defineNuxtConfig({
   alias:{
     "@": resolve(__dirname, "/")
   },
+
   devtools: { enabled: true },
+
   modules: [
     '@pinia/nuxt',
     '@pinia-plugin-persistedstate/nuxt',
     "@nuxt/ui",
     "dayjs-nuxt"
   ],
+
   pinia: {
     storesDirs: ['./stores/**', './custom-folder/stores/**'],
   },
+
   dayjs: {
     plugins: ['utc'],
   },
+
   css: [
     "~/assets/main.scss"
   ],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   colorMode: {
     preference: 'light'
   },
+
   runtimeConfig: {
     public: {
       baseURL: 'http://localhost:8888/',
     },
   },
+
   ssr: false,
+  compatibilityDate: '2024-08-29',
 })
