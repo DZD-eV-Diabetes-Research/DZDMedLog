8316b44684a54af820d8435832e57fd5c3ae3b6c
JTaeger <justust89@gmail.com>
2024-09-05 15:09:36 +0200

    wip

--- MedLog/frontend/pages/login/oidc/index.vue
+++ MedLog/frontend/pages/login/oidc/index.vue
@@ -6,11 +6,18 @@ import { UIBaseCard } from '../../../.nuxt/components';
             <br>
             state : {{ route.query.state }}
         </UIBaseCard>
+        <UIBaseCard>
+            {{ response }}
+        </UIBaseCard>
     </Layout>
 </template>
 
 <script setup>
 
 const route = useRoute()
+const runtimeConfig = useRuntimeConfig();
+
+const {data:response} = await useFetch(`${runtimeConfig.public.baseURL}/auth/oidc/token/openid-connect?code=${route.query.code}&state=${route.query.state}`)
+
 
 </script>
