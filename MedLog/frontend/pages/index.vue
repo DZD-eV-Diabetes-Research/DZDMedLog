e6899805f1f92cd980979296d4de29ef4d1101bb
JTaeger <justust89@gmail.com>
2025-01-17 11:12:01 +0100

    wip

--- MedLog/frontend/pages/index.vue
+++ MedLog/frontend/pages/index.vue
@@ -1,43 +1,42 @@
 <template>
   <Layout>
     <UIBaseCard>
-      <div>
-        <UForm
-          :schema="schema"
-          :state="state"
-          class="space-y-4"
-          @submit="tokenStore.login(state.username, state.password)"
-        >
+      <UForm :schema="schema" :state="state" class="space-y-4"
+        @submit="tokenStore.login(state.username, state.password)">
         <div style="text-align: center">
           <h3 v-if="tokenStore.my_401" style="color:red">Wrong username or password</h3>
-        </div>  
-          <UFormGroup label="User Name" name="username">
-            <UInput v-model="state.username" />
-          </UFormGroup>
-          <UFormGroup label="Password" name="password">
-            <UInput v-model="state.password" type="password" />
-          </UFormGroup>
-          <div class="flex justify-center">
-            <UButton
-              color="green"
-              variant="soft"
-              class="border border-green-500 hover:bg-green-300 hover:border-white hover:text-white"
-              type="submit"
-            >
-              Submit
-            </UButton>
-          </div>
-        </UForm>
-        <p>
-          No account?
-          <a href="https://auth.dzd-ev.org/" target="_blank">Sign Up</a>
-        </p>
+        </div>
+        <UFormGroup label="Benutzername" name="username">
+          <UInput v-model="state.username" />
+        </UFormGroup>
+        <UFormGroup label="Passwort" name="password">
+          <UInput v-model="state.password" type="password" />
+        </UFormGroup>
+        <div class="flex justify-center">
+          <UButton color="green" variant="soft"
+            class="border border-green-500 hover:bg-green-300 hover:border-white hover:text-white" type="submit">
+            Einloggen
+          </UButton>
+        </div>
+      </UForm>
+      <div class="loginButtonContainer">
+        <UButton @click="loginWithAuth()" color="red" variant="soft" label="Login mit DZD Authentik"
+          class="border border-red-500 hover:bg-white hover:border-red-500 hover:text-red-500">
+          <template #trailing>
+            <img src="/public/icons/authentik-orange.svg" alt="DZD Authentik" style="width: 24px; height: 24px;" />
+          </template>
+        </UButton>
       </div>
+      <p>
+        Kein Account?
+        <a href="https://auth.dzd-ev.org/" target="_blank">Registrieren Sie sich hier.</a>
+      </p>
     </UIBaseCard>
   </Layout>
 </template>
 
 <script setup lang="ts">
+const runtimeConfig = useRuntimeConfig()
 const tokenStore = useTokenStore();
 tokenStore.my_401 = false
 
@@ -45,8 +44,8 @@ import { object, string, type InferType } from "yup";
 import type { FormSubmitEvent } from "#ui/types";
 
 const schema = object({
-  username: string().required("Required"),
-  password: string().required("Required"),
+  username: string().required("Benötigt"),
+  password: string().required("Benötigt"),
 });
 
 type Schema = InferType<typeof schema>;
@@ -56,4 +55,24 @@ const state = reactive({
   password: undefined,
 });
 
+async function loginWithAuth() {
+  try {
+    // await $fetch(`${runtimeConfig.public.baseURL}/auth/oidc/login/openid-connect`, {
+    //   method: "GET",
+    //   redirect: "follow"})
+    window.location.href = `${runtimeConfig.public.baseURL}/auth/oidc/login/openid-connect`
+    console.log(`${runtimeConfig.public.baseURL}/auth/oidc/login/openid-connect`);
+
+  } catch (error) {
+    console.log(error);
+
+  }
+}
+
 </script>
+
+<style scoped>
+.loginButtonContainer {
+  margin: 2%;
+}
+</style>
