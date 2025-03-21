9e725eeccdaf86a755bb8073c9d237d24e53fc38
JTaeger <justust89@gmail.com>
2025-03-21 09:57:20 +0100

    wip

--- MedLog/frontend/components/CopyPreviousDrugs/index.vue
+++ MedLog/frontend/components/CopyPreviousDrugs/index.vue
@@ -5,7 +5,7 @@
                 style="margin-right: 10px"
                 class="border border-green-500 hover:bg-green-300 hover:border-white hover:text-white" />
             <div class="flex items-center ">
-                <UTooltip :delay-duration="0" text="Medikatmente aus dem letzten Events übernehmen">
+                <UTooltip :delay-duration="0" text="Medikatmente aus dem letzten Event übernehmen">
                     <UIcon name="i-heroicons-question-mark-circle" class="size-5" />
                 </UTooltip>
             </div>
@@ -30,6 +30,11 @@
                 </div>
             </div>
         </UModal>
+        <p v-for="intake in previousIntakes">
+            intake: {{ intake }}
+            <br>
+            <br>
+        </p>
     </div>
 </template>
 
@@ -51,13 +56,13 @@ async function openCopyIntakeModal() {
     errorMessage.value = false
     try {
         //`${runtimeConfig.public.baseURL}study/${route.params.study_id}/proband/${route.params.proband_id}/interview/current/intake`
-        const intakes = await $fetch(`${runtimeConfig.public.baseURL}study/b6f2c61b-d388-4412-8c9a-461ece251116/proband/1234/interview/current/intake`, {
+        const intakes = await $fetch(`${runtimeConfig.public.baseURL}study/b326a6e1-c2d1-4761-8590-05ca50d4e851/proband/1234/interview/current/intake`, {
             method: "GET",
             headers: { 'Authorization': "Bearer " + tokenStore.access_token },
         })
-
+        
         previousIntakes.value = Array.isArray(intakes) ? intakes.map((intake: any) => ({
-            Medikament: intake.drug_id.substring(0, 10),
+            Medikament: intake.drug_id,
             Einnahmebeginn: intake.intake_start_time_utc || 'Unbekannt',
             Einnahmeende: intake.intake_end_time_utc || 'Unbekannt',
             Dosis: intake.dose_per_day || 'Unbekannt',
@@ -103,6 +108,19 @@ async function saveIntakes() {
             
     //     }
     // });
+    for (const element of selecteIntakes.value) {
+        console.log(element.postBody);
+        try {
+            //`${runtimeConfig.public.baseURL}study/${route.params.study_id}/interview/${route.params.interview_id}/intake`
+            await $fetch(`${runtimeConfig.public.baseURL}study/8713edbc-4190-4ccc-9c2d-21fc75883e77/interview/440ff1cb-0ab3-4a97-a8b5-789ab5830bc1/intake`, {
+            method: "POST",
+            headers: { 'Authorization': "Bearer " + tokenStore.access_token },
+        })
+
+        } catch (error) {
+            console.log(error);
+    }
+}
     loadingSpinner.value = false
     openCopyPreviousIntakesModal.value = false
 
