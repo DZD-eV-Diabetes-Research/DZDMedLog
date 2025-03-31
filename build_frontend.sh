041acc602f298ef54d945f9d3a39c2ccff1c4e1c
motey <bleimehl@gmail.com>
2025-03-31 18:08:42 +0200

    wip

--- build_frontend.sh
+++ build_frontend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # bun install && bun run build &&
+docker pull oven/bun 
 docker run -it --user $(id -u):$(id -g) -v ./MedLog/frontend:/app oven/bun /bin/sh -c "cd /app && bun install && bunx nuxi generate"
 # wrong file linke because it was made in docker
 rm ./MedLog/frontend/dist
