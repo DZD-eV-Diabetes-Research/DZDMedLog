263cb788d8715cb5a98610c977fb3974035649fb
motey <bleimehl@gmail.com>
2025-04-03 14:06:12 +0200

    refresh req files

--- build_requirement_files.sh
+++ build_requirement_files.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-python -m pip install pip-tools -U
 
-rm MedLog/backend/requirements.txt
-python -m piptools compile -o MedLog/backend/requirements.txt MedLog/backend/pyproject.toml
-
-rm MedLog/backend/requirements_tests.txt
-python -m piptools compile --extra=tests -o MedLog/backend/requirements_tests.txt MedLog/backend/pyproject.toml
-
-rm MedLog/backend/requirements_docs.txt
+docker run --rm -v "$(pwd)":/app -w /app python:latest bash -c "
+python -m pip install --user pip-tools -U &&
+rm -f MedLog/backend/requirements.txt &&
+python -m piptools compile -o MedLog/backend/requirements.txt MedLog/backend/pyproject.toml &&
+chown $(id -u):$(id -g) MedLog/backend/requirements.txt MedLog/backend/pyproject.toml &&
+rm -f MedLog/backend/requirements_tests.txt &&
+python -m piptools compile --extra=tests -o MedLog/backend/requirements_tests.txt MedLog/backend/pyproject.toml &&
+chown $(id -u):$(id -g) MedLog/backend/requirements_tests.txt
+rm -f MedLog/backend/requirements_docs.txt &&
 python -m piptools compile --extra=docs -o MedLog/backend/requirements_docs.txt MedLog/backend/pyproject.toml
+chown $(id -u):$(id -g) MedLog/backend/requirements_docs.txt
+"
