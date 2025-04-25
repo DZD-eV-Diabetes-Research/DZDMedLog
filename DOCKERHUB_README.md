3eda6dc7b9ba96524d5a8fbb24b36cc021a44478
motey <bleimehl@gmail.com>
2025-04-25 10:19:54 +0200

    try fix #67

--- DOCKERHUB_README.md
+++ DOCKERHUB_README.md
@@ -14,14 +14,14 @@ Get or Update the container image
 
 Run the container
 
-`docker run -v ./database:/opt/medlog/data -p 8888:8888 -e DEMO_MODE=true dzdde/dzdmedlog`
+`docker run -p 8888:8888 -e DEMO_MODE=true dzdde/dzdmedlog`
 
 (Alternatively) If you have a WiDo GKV Arzneimittelindex to hand:
 
-`docker run -v ./database:/opt/medlog/data ./GKV_AI_StammPlus:/opt/medlog/arzneimittelindex -p 8888:8888 -e DEMO_MODE=true dzdde/dzdmedlog`
+`docker run -p 8888:8888 -e DEMO_MODE=true dzdde/dzdmedlog`
 
 visit http://localhost:8888
 
 for a local build:
 
-`docker run -v ./database:/opt/medlog/data -v ./GKV_AI_StammPlus:/opt/medlog/arzneimittelindex -p 8888:8888 -e DRUG_TABLE_PROVISIONING_SOURCE_DIR=/opt/medlog/arzneimittelindex/202301 -e DEMO_MODE=true --rm <image_name>`
+`docker run -p 8888:8888 -e DEMO_MODE=true --rm <image_name>`
--- MedLog/backend/medlogserver/api/routes/routes_webclient.py
+++ MedLog/backend/medlogserver/api/routes/routes_webclient.py
@@ -274,6 +274,7 @@ else:
     # We use the compiled static file client
     @fast_api_webclient_router.get("/{path_name:path}")
     async def serve_frontend(path_name: Optional[str] = None):
+        headers = {}
 
         full_path = Path(config.FRONTEND_FILES_DIR, path_name)
         log.debug(f"request frontend path {path_name} {full_path.is_file()}")
@@ -284,6 +285,15 @@ else:
         else:
             file = os.path.join(config.FRONTEND_FILES_DIR, path_name, "index.html")
         if Path(file).exists():
-            return FileResponse(file)
+            if path_name.endswith(".css"):
+                headers["content-type"] = "text/css; charset=UTF-8"
+            elif path_name.endswith(".js"):
+                headers["content-type"] = "application/javascript; charset=UTF-8"
+            elif path_name.endswith(".html"):
+                headers["content-type"] = "text/html; charset=UTF-8"
+            elif path_name.endswith(".json"):
+                headers["content-type"] = "application/json; charset=UTF-8"
+            return FileResponse(file, headers=headers)
         # SPA Fallback. Let the Nuxt Client router parse URL
-        return FileResponse(f"{config.FRONTEND_FILES_DIR}/index.html")
+        headers["content-type"] = "text/html; charset=UTF-8"
+        return FileResponse(f"{config.FRONTEND_FILES_DIR}/index.html", headers=headers)
