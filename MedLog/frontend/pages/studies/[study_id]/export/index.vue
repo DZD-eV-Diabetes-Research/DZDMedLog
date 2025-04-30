6dad0b50ad3aff50805bc44b4a68276590afe334
JTaeger <justust89@gmail.com>
2025-04-30 11:03:03 +0200

    added failed state icon and hove over all statuses

--- MedLog/frontend/pages/studies/[study_id]/export/index.vue
+++ MedLog/frontend/pages/studies/[study_id]/export/index.vue
@@ -4,49 +4,38 @@
       <h2 class="text-4xl font-normal mb-4">Datenexport</h2>
     </div>
     <div style="text-align: center">
-      <UButton
-        type="button"
-        label="Download anfragen"
-        color="blue"
-        variant="soft"
-        @click="requestDownload()"
-        class="border border-blue-500 hover:bg-blue-300 hover:border-white hover:text-white"
-      />
+      <UButton type="button" label="Download anfragen" color="blue" variant="soft" @click="requestDownload()"
+        class="border border-blue-500 hover:bg-blue-300 hover:border-white hover:text-white" />
     </div>
     <div class="my-card">
       <UTable :rows="downloads" :columns="columns">
         <template #status-data="{ row }">
-          <UButton
-            v-if="row.status === 'success'"
-            icon="i-heroicons-check"
-            size="2xs"
-            color="primary"
-            variant="solid"
-            :ui="{ rounded: 'rounded-full' }"
-            class="no-hover"
-            square
-          />
+          <div v-if="row.status === 'success'">
+            <UTooltip text="Export erfolgreich" :popper="{ arrow: true }">
+            <UButton icon="i-heroicons-check" size="2xs" color="primary" variant="solid"
+              :ui="{ rounded: 'rounded-full' }" class="no-hover" square />
+            </UTooltip>
+          </div>
 
-          <UButton
-            v-else
-            icon="i-heroicons-arrow-path"
-            size="2xs"
-            color="blue"
-            variant="outline"
-            :ui="{ rounded: 'rounded-full' }"
-            :class="{ rotating: row.status === 'queued' }"
-            class="no-hover"
-            square
-          />
+          <div v-else-if="row.status === 'failed'">
+            <UTooltip text="Export fehlgeschlagen" :popper="{ arrow: true }">
+              <UButton icon="i-heroicons-x-mark" size="2xs" color="red" variant="solid"
+                :ui="{ rounded: 'rounded-full' }" class="no-hover" square />
+            </UTooltip>
+          </div>
+
+          <div v-else>
+            <UTooltip text="Export läuft" :popper="{ arrow: true }">
+              <UButton icon="i-heroicons-arrow-path" size="2xs" color="blue" variant="outline"
+              :ui="{ rounded: 'rounded-full' }" :class="{ rotating: row.status === 'queued' }" class="no-hover"
+              square />
+            </UTooltip>
+
+          </div>
         </template>
         <template #actions-data="{ row }">
-          <UButton
-            v-if="row.status === 'success'"
-            color="gray"
-            variant="ghost"
-            icon="i-heroicons-arrow-down-tray"
-            @click="downloadFile(row)"
-          />
+          <UButton v-if="row.status === 'success'" color="gray" variant="ghost" icon="i-heroicons-arrow-down-tray"
+            @click="downloadFile(row)" />
         </template>
       </UTable>
     </div>
@@ -83,24 +72,23 @@ const columns = [
 const downloads = ref([]);
 let downloadCheckInterval: NodeJS.Timeout | null = null;
 
-function parseTime(time:string) {
-    const date = new Date(time);
-    const options = {
-        weekday: 'short',
-        year: 'numeric',
-        month: 'short',
-        day: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false,
-    };
-    return date.toLocaleString('de-DE', options).replace(',', '');
+function parseTime(time: string) {
+  const date = new Date(time);
+  const options = {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  };
+  return date.toLocaleString('de-DE', options).replace(',', '');
 }
 
 
-async function getDownloads() {
-  const data = await $api(
-    `${runtimeConfig.public.baseURL}study/${route.params.study_id}/export`);
+async function listDownloads() {
+  const data = await $api(`${runtimeConfig.public.baseURL}study/${route.params.study_id}/export`);
 
   const studyName = await studyStore.getStudy(route.params.study_id);
 
@@ -149,7 +137,7 @@ async function requestDownload() {
         method: "POST",
       }
     );
-    getDownloads();
+    listDownloads();
     startDownloadCheck();
 
   } catch (error) {
@@ -159,11 +147,11 @@ async function requestDownload() {
 
 function startDownloadCheck() {
   if (!downloadCheckInterval) {
-    downloadCheckInterval = setInterval(getDownloads, 5000); 
+    downloadCheckInterval = setInterval(listDownloads, 5000);
   }
 }
 
-getDownloads();
+listDownloads();
 
 onBeforeUnmount(() => {
   if (downloadCheckInterval) {
@@ -177,6 +165,7 @@ onBeforeUnmount(() => {
   from {
     transform: rotate(0deg);
   }
+
   to {
     transform: rotate(180deg);
   }
@@ -199,8 +188,8 @@ onBeforeUnmount(() => {
 }
 
 .no-hover:hover {
-  background-color: transparent; 
-  border-color: inherit; 
+  background-color: transparent;
+  border-color: inherit;
   color: inherit;
 }
 </style>
