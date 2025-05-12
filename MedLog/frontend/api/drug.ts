9d15a3c9a57677f10a026598476f82219e0e64e6
JTaeger <justust89@gmail.com>
2025-05-12 14:54:30 +0200

    fiex missing hover info

--- MedLog/frontend/api/drug.ts
+++ MedLog/frontend/api/drug.ts
@@ -1,19 +1,17 @@
 export async function apiGetFieldDefinitions(type: string) {
     const runTimeConfig = useRuntimeConfig();
-    const tokenStore = useTokenStore();
     const { $api } = useNuxtApp();
 
 
     try {
-        const response = await $api(`${runTimeConfig.public.baseURL}drug/field_def`);
-
-        const filterFn = (item: any) => type === 'search_result' ? item.optional === false : true
+        const response = await $api(`${runTimeConfig.public.baseURL}drug/field_def`);        
+        // const filterFn = (item: any) => type === 'search_result' ? item.optional === false : true
 
         // Once Everything is set and fixed
-        //const filterFn = (item: any) => 
-        // type === 'search_result' ? item.show_in_search_result === true :
-        // type === 'dynamic_form' ? item.used_for_custom_drug === true :
-        // true;
+        const filterFn = (item: any) => 
+        type === 'search_result' ? item.show_in_search_results === true :
+        type === 'dynamic_form' ? item.used_for_custom_drug === true :
+        true;
 
         const categorizedList = {
             attrs: response.attrs?.filter(filterFn).map(item => [item.field_name_display, item.field_name, item.value_type]) || [],
@@ -21,7 +19,7 @@ export async function apiGetFieldDefinitions(type: string) {
             attrs_multi: response.attrs_multi?.filter(filterFn).map(item => [item.field_name_display, item.field_name, item.value_type]) || [],
             attrs_multi_ref: response.attrs_multi_ref?.filter(filterFn).map(item => [item.field_name_display, item.field_name, item.value_type]) || [],
         };
-
+        
         return categorizedList;
 
     } catch (error) {
