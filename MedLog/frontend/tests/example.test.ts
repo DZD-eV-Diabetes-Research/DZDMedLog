737c0f27c705f87c6fc7cd7dd985295f8324dfa5
JTaeger <justust89@gmail.com>
2025-07-24 13:12:19 +0200

    wip

--- MedLog/frontend/tests/example.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@nuxt/test-utils/playwright'
-
-test('test', async ({ page, goto }) => {
-    await goto('/login', { waitUntil: 'hydration' })
-    
-    await expect(page.getByRole('heading', { name: 'Der Backendstatus ist' }))
-})
