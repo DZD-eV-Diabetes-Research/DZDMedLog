f6cd336a8d14d6272269aef7273a2e843a87f5b7
motey <bleimehl@gmail.com>
2025-08-12 14:49:04 +0200

    minor refactor

--- MedLog/backend/medlogserver/api/routes/routes_auth.py
+++ MedLog/backend/medlogserver/api/routes/routes_auth.py
@@ -55,7 +55,7 @@ from medlogserver.api.auth.utils import (
     get_userinfo_from_token_or_endpoint,
     get_access_token_expires_at_value_from_token,
     generate_client_session_id,
-    revoke_token,
+    revoke_oidc_token,
 )
 
 from medlogserver.config import Config
@@ -429,7 +429,7 @@ async def logout(
             # revoke token
 
             oauth_client = oauth_clients[current_user_auth.oidc_provider_slug]
-            await revoke_token(oauth_client=oauth_client, token=refresh_token)
+            await revoke_oidc_token(oauth_client=oauth_client, token=refresh_token)
 
     elif current_user_auth.auth_source_type == AllowedAuthSchemeType.api_token:
         await user_auth_crud.delete(id=current_user_auth.id)
