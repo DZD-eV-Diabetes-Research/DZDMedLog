8d347d9040fa402b294df5060455fbd6d357811d
Andreas Brain <andreas.brain@helmholtz-munich.de>
2025-10-09 14:57:43 +0200

    fix: Fix URLs for imprint and repository

--- MedLog/frontend/components/Layout/Footer.vue
+++ MedLog/frontend/components/Layout/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="bg-gray-100 w-full py-4 mt-6">
     <div class="flex justify-end items-center py-2 px-10 mx-auto">
       <div>
-        <a href="https://www.dzd-ev.de/footermenue/impressum/index.html" target="_blank" class="text-xl text-black hover:text-blue-500 hover:underline">
+        <a href="https://www.dzd-ev.de/impressum" target="_blank" class="text-xl text-black hover:text-blue-500 hover:underline">
           Imprint
         </a>
       </div>
