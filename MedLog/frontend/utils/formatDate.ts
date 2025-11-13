dbb6b1f001cc4ce848b9c06d6122d8bff53de132
Andreas Brain <andreas.brain@helmholtz-munich.de>
2025-11-13 15:04:49 +0100

    fix: Show date in correct time zone

--- MedLog/frontend/utils/formatDate.ts
+++ MedLog/frontend/utils/formatDate.ts
@@ -1,5 +1,5 @@
 export default function (value: string) {
-    const date = new Date(value);
+    const date = new Date(value + 'Z'); // append Z to force parsing as UTC
 
     if (isNaN(date.valueOf())) {
         return date.toString();
