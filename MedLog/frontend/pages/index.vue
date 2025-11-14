e8445c84aa55c9b0a689ec81a309bedecc0cd310
Andreas Brain <andreas.brain@helmholtz-munich.de>
2025-11-14 15:05:46 +0100

    fix: Show proper warning if no studies exist

--- MedLog/frontend/pages/index.vue
+++ MedLog/frontend/pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="mt-8 w-11/12 lg:w-8/12 xl:w-6/12 mx-auto">
-    <h1 class="text-4xl font-normal text-center">Übersicht</h1>
+    <h1 class="text-4xl font-normal text-center mb-4">Übersicht</h1>
 
-    <ul v-if="studyStore.studies" class="mt-4">
+    <ul v-if="studyStore.studies.length" class="mt-4">
       <StudyOverviewCard v-for="study in studyStore.studies" :key="study.id" :study="study" as="li" />
     </ul>
-    <UAlert v-else-if="!userStore.isAdmin" title="Keine Studien verfügbar" description="Dies kann auch an fehlenden Rechten liegen" />
-    <UAlert v-else description="Es sind keine Studien verfügbar" />
+    <UAlert v-else-if="!userStore.isAdmin" color="yellow" title="Keine Studien verfügbar" description="Dies kann auch an fehlenden Rechten liegen." />
+    <UAlert v-else color="yellow" title="Keine Studien verfügbar" description="Über die Studienverwaltung können neue Studien angelegt werden." />
   </div>
 </template>
 
