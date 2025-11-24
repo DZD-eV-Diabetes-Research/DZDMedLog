39269dd8934be7293f1a6069da2cfaeec5c4c655
Andreas Brain <andreas.brain@helmholtz-munich.de>
2025-11-24 14:49:24 +0100

    feat: Add table of contents to help page

--- MedLog/frontend/pages/help/index.vue
+++ MedLog/frontend/pages/help/index.vue
@@ -6,16 +6,37 @@ const configStore = useConfigStore();
   <section class="container w-11/12 lg:w-8/12 xl:w-6/12 mx-auto mt-8">
     <h1 class="text-4xl font-normal text-center mb-4">Hilfe zu DZDMedLog</h1>
 
+    <UCard class="mb-4">
+      <h2 class="text-lg font-semibold mb-2">Inhaltsverzeichnis</h2>
+      <ul class="list-inside">
+        <li>
+          <ULink to="#studien" class="hover:underline">
+            Teil 1: Studien und Interviews erstellen bzw. verwalten
+          </ULink>
+        </li>
+        <li>
+          <ULink to="#interview" class="hover:underline">
+            Teil 2: Medikamente zu einem Interview hinzufügen
+          </ULink>
+        </li>
+        <li>
+          <ULink to="#export" class="hover:underline">
+            Teil 3: Daten einer Studie exportieren
+          </ULink>
+        </li>
+      </ul>
+    </UCard>
+
     <p>DZDMedLog besteht im Kern aus 3 Komponenten:</p>
 
-    <h2 class="text-xl mt-6 mb-2">Teil 1: Studien und Interviews erstellen bzw. verwalten</h2>
+    <h2 id="studien" class="text-xl mt-6 mb-2">Teil 1: Studien und Interviews erstellen bzw. verwalten</h2>
     <p>
       Teil 1 finden Sie mittels des Buttons "Studienverwaltung" auf der Landingpage.
       Diese können Sie von überall erreichen, indem Sie auf den DZD-Medlog-Schriftzug in der oberen linken Ecke klicken.
       Bitte beachten Sie, dass nur Benutzer mit Admin-Rechten neue Studien und Interviews anlegen können.
     </p>
 
-    <h2 class="text-xl mt-6 mb-2">Teil 2: Medikamente zu einem Interview hinzufügen</h2>
+    <h2 id="interview" class="text-xl mt-6 mb-2">Teil 2: Medikamente zu einem Interview hinzufügen</h2>
     <p>
       Teil 2 wird über den Button "Interview durchführen" erreicht.
       Hier wird eine von Ihnen gewählte Probanden-ID verlangt.
@@ -26,7 +47,7 @@ const configStore = useConfigStore();
       Sie haben die Möglichkeit, über den Button "Medikationsübernahme" die Medikamente <strong>des letzten abgeschlossenen Events</strong> zu übernehmen.
     </p>
 
-    <h2 class="text-xl mt-6 mb-2">Teil 3: Daten einer Studie exportieren</h2>
+    <h2 id="export" class="text-xl mt-6 mb-2">Teil 3: Daten einer Studie exportieren</h2>
     <p>
       Teil 3, den <strong>Datenexport</strong>, erreichen Sie, wie die Verwaltung der Studien, über den Button "Studienverwaltung" von der Landingpage aus.
     </p>
