6cae45b51dbd6db636fa2900000961e2c399d57a
Andreas Brain <andreas.brain@helmholtz-munich.de>
2026-03-11 15:31:39 +0100

    Fix types

--- MedLog/frontend/type-helper.ts
+++ MedLog/frontend/type-helper.ts
@@ -1,8 +1,8 @@
 import type {
     SchemaDrugAttrFieldDefinitionContainer,
-    SchemaMedlogserverModelDrugDataApiDrugModelFactoryAttrRefs,
-    SchemaMedlogserverModelDrugDataApiDrugModelFactoryAttrsMulti,
-    SchemaMedlogserverModelDrugDataApiDrugModelFactoryMultiAttrRefs
+    SchemaMedlogserverModelDrugDataApiDrugModelFactoryAttrRefs_1,
+    SchemaMedlogserverModelDrugDataApiDrugModelFactoryAttrsMulti_1,
+    SchemaMultiAttrRefs
 } from "#open-fetch-schemas/medlogapi";
 
 type ValueOf<T> = T[keyof T];
@@ -11,14 +11,14 @@ type ElementUnionOfArrayProps<T> = ElementType<ValueOf<T>>;
 
 export type FieldDefinition = ElementUnionOfArrayProps<SchemaDrugAttrFieldDefinitionContainer>
 
-export function isMultiValueField(fieldDefinition: FieldDefinition, _fields: object): _fields is SchemaMedlogserverModelDrugDataApiDrugModelFactoryAttrsMulti {
+export function isMultiValueField(fieldDefinition: FieldDefinition, _fields: object): _fields is SchemaMedlogserverModelDrugDataApiDrugModelFactoryAttrsMulti_1 {
     return fieldDefinition.is_multi_val_field && !fieldDefinition.is_reference_list_field;
 }
 
-export function isMultiRefField(fieldDefinition: FieldDefinition, _fields: object): _fields is SchemaMedlogserverModelDrugDataApiDrugModelFactoryMultiAttrRefs {
+export function isMultiRefField(fieldDefinition: FieldDefinition, _fields: object): _fields is SchemaMultiAttrRefs {
     return fieldDefinition.is_multi_val_field && fieldDefinition.is_reference_list_field;
 }
 
-export function isSingleRefField(fieldDefinition: FieldDefinition, _fields: object): _fields is SchemaMedlogserverModelDrugDataApiDrugModelFactoryAttrRefs {
+export function isSingleRefField(fieldDefinition: FieldDefinition, _fields: object): _fields is SchemaMedlogserverModelDrugDataApiDrugModelFactoryAttrRefs_1 {
     return !fieldDefinition.is_multi_val_field && fieldDefinition.is_reference_list_field;
 }
