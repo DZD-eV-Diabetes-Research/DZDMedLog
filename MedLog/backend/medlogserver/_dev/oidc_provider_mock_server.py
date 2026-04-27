ed45d615df09dfc998da6e7867a1674aefb7d6d6
motey <bleimehl@gmail.com>
2026-04-27 16:23:33 +0200

    fix refresh token mechanism

--- MedLog/backend/medlogserver/_dev/oidc_provider_mock_server.py
+++ MedLog/backend/medlogserver/_dev/oidc_provider_mock_server.py
@@ -1,6 +1,7 @@
 # https://oidc-provider-mock.readthedocs.io/latest/
 from typing import List, Dict
 import oidc_provider_mock
+from oidc_provider_mock import User
 import logging
 import time
 import requests
@@ -27,11 +28,29 @@ def provision_test_users(oidc_mockup_server_base_url: str):
         res.raise_for_status()
 
 
+def get_test_users() -> List[User]:
+    users: List[Dict] = []
+    if CONFIG_DATABASE_FILE.is_file():
+        with open(CONFIG_DATABASE_FILE, "r") as file:
+            file_content_parsed = yaml.safe_load(file.read())
+            if "users" in file_content_parsed:
+                users.extend(file_content_parsed["users"])
+    result_users: List[User] = []
+    for user in users:
+        print(f"Create testuser data: {user}")
+        result_users.append(User(sub=user["sub"], claims=user))
+    return result_users
+
+
 def start_oidc_server_thread(port: int = 8884):
-    with oidc_provider_mock.run_server_in_thread(port=port) as server:
+
+    with oidc_provider_mock.run_server_in_thread(
+        port=port,
+        user_claims=get_test_users(),
+    ) as server:
         server_url = f"http://localhost:{server.server_port}"
         print(f"OIDC Provider Mockup Server listening at {server_url}")
-        provision_test_users(server_url)
+        # provision_test_users(server_url)
         try:
             if __name__ == "__main__":
                 print("Press Ctrl+C to exit the loop.")
