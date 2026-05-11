f5ddf42024237aa087e582dd2fe8a1c5d92007e7
Andreas Brain <andreas.brain@helmholtz-munich.de>
2026-05-11 12:25:22 +0200

    Split help into pages by topic

---
+++ MedLog/frontend/pages/help/interviews.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="container w-11/12 lg:w-8/12 xl:w-6/12 mx-auto mt-8">
+    <div class="flex flex-col items-center">
+      <h1 class="text-4xl font-normal mb-4">Hilfe zu DZDMedLog</h1>
+
+      <UBreadcrumb
+        :links="[{
+          label: 'Hilfe',
+          icon: 'i-heroicons-question-mark-circle',
+          to: '/help'
+        }, {
+          label: 'Interview durchführen',
+        }]"
+        class="self-start"
+      />
+
+      <UDivider class="my-2" size="sm" />
+      <h2 class="text-3xl font-normal mb-4">Interview durchführen</h2>
+    </div>
+
+    <p>
+      Um ein Interview durchzuführen oder zu bearbeiten, klicken Sie auf das “Home” Icon oder den Schriftzug “Interview durchführen" im linken oberen Bereich der Seite.
+      Um ein Interview durchzuführen, muss eine Proband:innen-ID eingegeben werden.
+      Falls die Proband:innen-ID noch unbekannt ist, wird der/die Proband:in neu angelegt.
+      Falls der/die Proband:in in der Datenbank bereits vorhanden ist, sehen Sie eine Medikationsübersicht des/der Proband:in über die gesamte Studie.
+      Jetzt haben Sie die Möglichkeit, ein Interview durchzuführen oder zu bearbeiten.
+    </p>
+    <p>
+      Über die Funktion "Medikamente übernehmen" können Sie die Medikamente des letzten abgeschlossenen Interviews für diese:n Proband:in übernehmen.
+    </p>
+  </section>
+</template>
+
+<style scoped>
+p ~ p {
+  margin-top: 0.6rem;
+}
+</style>
---
+++ MedLog/frontend/pages/help/manage-studies.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const configStore = useConfigStore();
+</script>
+
+<template>
+  <section class="container w-11/12 lg:w-8/12 xl:w-6/12 mx-auto mt-8">
+    <div class="flex flex-col items-center">
+      <h1 class="text-4xl font-normal mb-4">Hilfe zu DZDMedLog</h1>
+
+
+      <UBreadcrumb
+          :links="[{
+          label: 'Hilfe',
+          icon: 'i-heroicons-question-mark-circle',
+          to: '/help'
+        }, {
+          label: 'Studienverwaltung ',
+        }]"
+          class="self-start"
+      />
+
+      <UDivider class="my-2" size="sm" />
+      <h2 class="text-3xl font-normal mb-4">Studienverwaltung </h2>
+    </div>
+
+    <p>
+      Zur Studien- und Kontoverwaltung - inklusive Anlage der Interviews als Events -  gelangen Sie über die entsprechenden Reiter rechts oben auf der DZDMedLog Landingpage.
+      Die Landingpage können Sie von überall erreichen, indem Sie auf den Schriftzug <em>{{ configStore.appName }}</em> in der oberen linken Ecke klicken.
+    </p>
+
+    <WarningMessage
+      title=""
+      message="Bitte beachten Sie, dass nur Benutzer:innen mit Admin-Rechten neue Studien und Interviews anlegen können."
+      class="mt-4"
+    />
+  </section>
+</template>
+
+<style scoped>
+p ~ p {
+  margin-top: 0.6rem;
+}
+</style>
