bdf54e87e35a9c0209bc00b2824bc6398326a47f
Andreas Brain <andreas.brain@helmholtz-munich.de>
2026-05-11 16:52:50 +0200

    Show full help only when logged in

--- MedLog/frontend/pages/help/index.vue
+++ MedLog/frontend/pages/help/index.vue
@@ -1,78 +1,88 @@
 <script setup lang="ts">
 const configStore = useConfigStore();
+const userStore = useUserStore();
 </script>
 
 <template>
   <section class="container w-11/12 lg:w-8/12 xl:w-6/12 mx-auto mt-8">
     <h1 class="text-4xl font-normal text-center mb-4">Hilfe zu DZDMedLog</h1>
 
-    <p class="my-4 text-center text-gray-500">
-      Wählen Sie eines der Themengebiete, um mehr zu erfahren.
-    </p>
+    <template v-if="userStore.isLoggedIn">
+      <p class="my-4 text-center text-gray-500">
+        Wählen Sie eines der Themengebiete, um mehr zu erfahren.
+      </p>
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
-      <ULink to="/help/interviews">
-        <UCard class="border border-gray-400 hover:bg-gray-100">
-          <div class="flex flex-row items-center gap-2">
-            <UIcon name="i-heroicons-clipboard-document-check" class="w-8 h-8" />
-            Interview durchführen
-          </div>
-        </UCard>
-      </ULink>
-      <ULink to="/help/search">
-        <UCard class="border border-gray-400 hover:bg-gray-100">
-          <div class="flex flex-row items-center gap-2">
-            <UIcon name="i-heroicons-magnifying-glass" class="w-8 h-8" />
-            Hinweise zur Medikamentensuche
-          </div>
-        </UCard>
-      </ULink>
-      <ULink to="/help/manage-studies">
-        <UCard class="border border-gray-400 hover:bg-gray-100">
-          <div class="flex flex-row items-center gap-2">
-            <UIcon name="i-heroicons-clipboard-document-list" class="w-8 h-8" />
-            Studienverwaltung
-          </div>
-        </UCard>
-      </ULink>
-      <ULink to="/help/manage-users">
-        <UCard class="border border-gray-400 hover:bg-gray-100">
-          <div class="flex flex-row items-center gap-2">
-            <UIcon name="i-heroicons-users" class="w-8 h-8" />
-            Kontoverwaltung
-          </div>
-        </UCard>
-      </ULink>
-      <ULink to="/help/export">
-        <UCard class="border border-gray-400 hover:bg-gray-100">
-          <div class="flex flex-row items-center gap-2">
-            <UIcon name="i-heroicons-cloud-arrow-down" class="w-8 h-8" />
-            Datenexport
-          </div>
-        </UCard>
-      </ULink>
-    </div>
+      <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <ULink to="/help/interviews">
+          <UCard class="border border-gray-400 hover:bg-gray-100">
+            <div class="flex flex-row items-center gap-2">
+              <UIcon name="i-heroicons-clipboard-document-check" class="w-8 h-8" />
+              Interview durchführen
+            </div>
+          </UCard>
+        </ULink>
+        <ULink to="/help/search">
+          <UCard class="border border-gray-400 hover:bg-gray-100">
+            <div class="flex flex-row items-center gap-2">
+              <UIcon name="i-heroicons-magnifying-glass" class="w-8 h-8" />
+              Hinweise zur Medikamentensuche
+            </div>
+          </UCard>
+        </ULink>
+        <ULink to="/help/manage-studies">
+          <UCard class="border border-gray-400 hover:bg-gray-100">
+            <div class="flex flex-row items-center gap-2">
+              <UIcon name="i-heroicons-clipboard-document-list" class="w-8 h-8" />
+              Studienverwaltung
+            </div>
+          </UCard>
+        </ULink>
+        <ULink to="/help/manage-users">
+          <UCard class="border border-gray-400 hover:bg-gray-100">
+            <div class="flex flex-row items-center gap-2">
+              <UIcon name="i-heroicons-users" class="w-8 h-8" />
+              Kontoverwaltung
+            </div>
+          </UCard>
+        </ULink>
+        <ULink to="/help/export">
+          <UCard class="border border-gray-400 hover:bg-gray-100">
+            <div class="flex flex-row items-center gap-2">
+              <UIcon name="i-heroicons-cloud-arrow-down" class="w-8 h-8" />
+              Datenexport
+            </div>
+          </UCard>
+        </ULink>
+      </div>
 
-    <hr>
+      <hr>
 
-    <UAlert v-if="configStore.branding.supportEmail" color="sky" icon="i-heroicons-envelope" class="mt-4">
-      <template #description>
-        <p class="text-base">
-          Wenn Sie Fragen oder Feedback haben, melden Sie sich gerne:
-          <NuxtLink :to="`mailto:${configStore.branding.supportEmail}`" :external="true">
-            {{ configStore.branding.supportEmail }}
-          </NuxtLink>
-        </p>
-        <p>
-          Bei Fehlerberichten bitte die untenstehenden Versionsangaben einschließen.
-        </p>
-      </template>
-    </UAlert>
+      <UAlert v-if="configStore.branding.supportEmail" color="sky" icon="i-heroicons-envelope" class="mt-4">
+        <template #description>
+          <p class="text-base">
+            Wenn Sie Fragen oder Feedback haben, melden Sie sich gerne:
+            <NuxtLink :to="`mailto:${configStore.branding.supportEmail}`" :external="true">
+              {{ configStore.branding.supportEmail }}
+            </NuxtLink>
+          </p>
+          <p>
+            Bei Fehlerberichten bitte die untenstehenden Versionsangaben einschließen.
+          </p>
+        </template>
+      </UAlert>
 
-    <div class="text-center font-extralight mt-4">
-      <p>Version: {{ configStore.versionInfo.version ?? 'Unbekannt' }}</p>
-      <p>Branch: {{ configStore.versionInfo.branch ?? 'Unbekannt' }}</p>
-    </div>
+      <div class="text-center font-extralight mt-4">
+        <p>Version: {{ configStore.versionInfo.version ?? 'Unbekannt' }}</p>
+        <p>Branch: {{ configStore.versionInfo.branch ?? 'Unbekannt' }}</p>
+      </div>
+    </template>
+    <template v-else>
+      <WarningMessage
+          title=""
+          message="DZDMedLog ist eine interne, vom DZD entwickelte Software zur interoperablen Dokumentation von Medikation. Zur Nutzung müssen Sie als Nutzer:in registriert und eingeloggt sein."
+          class="mt-4"
+      />
+    </template>
 
     <hr>
 
